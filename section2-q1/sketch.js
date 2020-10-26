c48f14d51a6fdefdd2471b66ff22335f2fbf4916
1991015H <1991015h@stu.kobe-u.ac.jp>
2020-10-26 21:12:09 +0900

    section2-q1 update,section2-q2 update

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,17 @@
 // 小手調べ
 function setup(){
+  createCanvas(200,200);
+}
+
+function draw(){
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    ellipse(100,100,(i+1) * 10);
+    noFill();
+    if (i>=4 && i<=8) {
+      stroke(255,0,0);
+    }
+    else {
+      stroke(0,0,255);
+    }
   }
 }
