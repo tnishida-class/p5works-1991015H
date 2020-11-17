e48faf0c27fe454f6d068390f3d2ce6760c28d18
1991015H <1991015h@stu.kobe-u.ac.jp>
2020-11-17 11:14:54 +0900

    section4-q1 update

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -6,11 +6,14 @@ function setup(){
   createCanvas(200, 200);
   count = 0;
   cycle = 100;
+  size = 30;
 }
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
-  // BLANK[1]
+  if(size > 30){size = 30 + count;}
+  if(size < 80){size = 130 - count;}
   ellipse(width / 2, height / 2, size);
+  if(keyIsDown(UP_ARROW)){count = (count + 2) % cycle};
 }
