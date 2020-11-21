0bb3733ddccb7b558934442a1dc250086a9e156d
1991015H <1991015h@stu.kobe-u.ac.jp>
2020-11-21 18:58:50 +0900

    section4-q2 update

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -3,27 +3,36 @@ let x, y, vx, vy;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  x = width / 2;
-  y = height / 2;
   vx = 8;
   vy = 8;
+  angle = 0;
+  fill("yellow");
+  noStroke();
 }
 
 function draw(){
   background(160, 192, 255);
-  // BLANK[2] (hint: 作った star 関数を使います)
-
-  // 端の処理パターン (1) 反対側から出てくる
+   star(x , y , 20, angle);
+  angle += 0.05;
+  x -= vx;
+  y += vy;
   if(x > width){ x = 0; }
   else if(x < 0){ x = width; }
   if(y > height){ y = 0; }
   if(y < 0){ y = height; }
+  if(keyIsDown(DOWN_ARROW)){ vy = 16}
+  if(keyIsDown(UP_ARROW)){ vy = 8}
+}
+
+function mouseClicked(){
+  x = mouseX;
+  y = mouseY;
 }
 
 function star(cx, cy, r, angle){
   beginShape();
   for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI 　+ angle ;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
