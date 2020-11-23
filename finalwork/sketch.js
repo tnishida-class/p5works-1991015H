9df30e8460e6f147f00415d315642be4d030451d
1991015H <1991015h@stu.kobe-u.ac.jp>
2020-11-24 02:26:24 +0900

    finalwork update

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,11 +1,85 @@
 // 最終課題を制作しよう
+let x, y, vx, vy;
+let grabbed;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
+  reset();
+}
+
+function reset(){
+  x = width / 2;
+  y = height / 2;
+  vx = 5;
+  vy = 5;
+  grabbed = false;
+  textSize(60);
+  textAlign(CENTER);
+  fill(160,192,255);
+  noStroke();
 }
 
 function draw(){
   background(160, 192, 255);
+
+  if(!grabbed){
+  fill("black");
+  text("Where is the hexagone?",width / 2,height / 2);
+
+  x += vx;
+  y += vy;
+
+  if(x < 0 || x > width){ vx = -1 * vx; }
+  if(y < 0 || y > height){ vy = -1 * vy; }
+
+  x = constrain(x, 0, width);
+  y = constrain(y, 0, height);
+
+  if(keyIsDown(LEFT_ARROW)){ vx = 10 ; }
+  if(keyIsDown(UP_ARROW)){ vy = 10; }
+}
+else{
+  background(255,255,255);
+  text("Clear!", width / 2, height /2);
+}
+
+if(keyIsDown(" ".charCodeAt(0))){ background(255,255,255); }
+fill(160,192,255);
+hexagone(x , y , 60);
+}
+
+function mousePressed(){
+  grabbed = dist(mouseX, mouseY, x, y) < 80;
+}
+
+function mouseDragged(){
+  if(grabbed){
+    x = mouseX;
+    y = mouseY;
+  }
+}
+
+function mouseReleased(){
+  if(grabbed){
+    grabbed = false;
+    vx = mouseX - pmouseX;
+    vy = mouseY - pmouseY;
+  }
+}
+
+function keyPressed(){
+  if(key == "r"){ reset(); }
+}
+
+function hexagone(cx, cy, r){
+  beginShape();
+  for(var i = 0; i < 20; i++){
+    var theta = TWO_PI * i * 1 / 6 - HALF_PI  ;
+    var x = cx + cos(theta) * r;
+    var y = cy + sin(theta) * r;
+    vertex(x,y);
+  }
+  endShape(CLOSE);
 }
 
 function windowResized(){
--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,16 +5,16 @@ function setup() {
 }
 
 function draw(){
-  const step = 25; //各ブロックの間隔
+  const step = 25;
 
   for (let y = 0; y <= height; y += step) {
-    if(y % (step*2) == 0){  // y の値が (step*2) で割り切れる時
+    if(y % (step*2) == 0){
       for (let x = step ; x <= width; x += step*2) {
         noStroke();
         fill('gray');
         rect(x, y, step, step);
       }
-    }else{　 // y の値が (step*2) で割り切れない時
+    }else{　
       for (let x = 0; x <= width; x += step*2) {
         noStroke();
         fill('gray');
@@ -24,13 +24,13 @@ function draw(){
   }
 
   for (let y = 12.5; y <= 75; y += step) {
-    if(y % 37.5 != 0){  // y の値が (step*2) で割り切れる時
+    if(y % 37.5 != 0){
       for (let x = 37.5 ; x <= width; x += step*2) {
         noStroke();
         fill('red');
         circle(x, y, 20 );
       }
-    }else{　 // y の値が (step*2) で割り切れない時
+    }else{　 
       for (let x = 12.5; x <= width; x += step*2) {
         noStroke();
         fill('red');
